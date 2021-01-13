fc29ca9e68864cef6b2e3d62cd0fd95d6ba93ad2
Nathan Sutherland <nathan.sutherland17@gmail.com>
2021-01-13 11:55:54 -0700

    done with VSchool

--- rtv/models/user.js
+++ rtv/models/user.js
@@ -1,17 +1,44 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 const Schema = mongoose.Schema
+const bcrypt = require("bcrypt")
 
 const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    lowercase: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
+	username: {
+		type: String,
+		required: true,
+		lowercase: true,
+		unique: true
+	},
+	password: {
+		type: String,
+		required: true
+	}
 })
+
+//Encrypt user passwords on signup
+userSchema.pre("save", function (next) {
+	const user = this
+	if (!user.isModified("password")) return next()
+	bcrypt.hash(user.password, 10, (err, hash) => {
+		if (err) return next(err)
+		user.password = hash
+		next()
+	})
+})
+
+//Method to check encrypted password on login
+userSchema.methods.checkPassword = function (passwordAttempt, callback) {
+	bcrypt.compare(passwordAttempt, this.password, (err, isMatch) => {
+		if (err) return callback(err)
+		return callback(null, isMatch)
+	})
+}
+
+//Method to remove user's password
+userSchema.methods.withoutPassword = function () {
+	const user = this.toObject()
+	delete user.password
+	return user
+}
 
 module.exports = mongoose.model("User", userSchema)
--- rtv/server.js
+++ rtv/server.js
@@ -1,36 +1,40 @@
-const express = require('express')
+const express = require("express")
 const app = express()
 require("dotenv").config()
-const morgan = require('morgan')
-const mongoose = require('mongoose')
-const expressJwt = require('express-jwt')
+const morgan = require("morgan")
+const mongoose = require("mongoose")
+const expressJwt = require("express-jwt")
 
 app.use(express.json())
-app.use(morgan('dev'))
+app.use(morgan("dev"))
 
-mongoose.connect('mongodb://localhost:27017/auth',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    },
-    () => console.log('Connected to the DB')
+mongoose.connect(
+	"mongodb://localhost:27017/auth",
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+		useFindAndModify: false
+	},
+	() => console.log("Connected to the DB")
 )
 
-app.use('/auth', require('./routes/authRouter.js'))
-app.use('/api', expressJwt({ secret: process.env.SECRET, algorithms: ['HS256'] }))
-app.use('/api/issue', require('./routes/issueRouter.js'))
+app.use("/auth", require("./routes/authRouter.js"))
+app.use(
+	"/api",
+	expressJwt({ secret: process.env.SECRET, algorithms: ["HS256"] })
+)
+app.use("/api/issue", require("./routes/issueRouter.js"))
 app.use("/api/comment", require("./routes/commentRouter.js"))
 
 app.use((err, req, res, next) => {
-  console.log(err)
-  if(err.name === "UnauthorizedError"){
-    res.status(err.status)
-  }
-  return res.send({errMsg: err.message})
+	console.log(err)
+	if (err.name === "UnauthorizedError") {
+		res.status(err.status)
+	}
+	return res.send({ errMsg: err.message })
 })
 
 app.listen(9052, () => {
-  console.log(`Server is running on local port 9052`)
+	console.log(`Server is running on local port 9052`)
 })
